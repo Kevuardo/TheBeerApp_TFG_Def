f2f56c253f84bf8dc8dcbf7e55a1e146b88768b9
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-11-15 21:04:00 +0100

    Activity de la SplashScreen (la del inicio) + layout

---
+++ TFG/app/src/main/java/com/kcastilloe/thebeerapp_def/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.kcastilloe.thebeerapp_def;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 2000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
