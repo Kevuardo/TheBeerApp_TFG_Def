5f68df059a4cb260527dd31b2b028419c4626373
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-11-16 20:41:53 +0100

    Activity que mostrará los detalles de las cervezas

---
+++ TFG/app/src/main/java/com/kcastilloe/thebeerapp_def/DetalleCervezaActivity.java
@@ -0,0 +1,13 @@
+package com.kcastilloe.thebeerapp_def;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalleCervezaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_cerveza);
+    }
+}
