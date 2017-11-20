ca8d2086ea96e9017ecfcb048eaae8a0b841bfa1
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-11-20 01:39:22 +0100

    Clase modelo de los usuarios de la BDD

---
+++ TFG/app/src/main/java/com/kcastilloe/thebeerapp_def/modelo/Usuario.java
@@ -0,0 +1,13 @@
+package com.kcastilloe.thebeerapp_def.modelo;
+
+/**
+* Clase modelo dedicada a los usuarios que se darán de alta en la app.
+*
+* @author Kevin Castillo Escudero
+*/
+
+public class Usuario {
+
+
+
+}
