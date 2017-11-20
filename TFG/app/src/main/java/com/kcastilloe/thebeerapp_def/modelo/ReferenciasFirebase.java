7d3527db514e12c4ea30dfeffe437da4d7aeb37f
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-11-20 11:34:47 +0100

    Clase modelo par almacenar las referencias a los nodos de la BDD

---
+++ TFG/app/src/main/java/com/kcastilloe/thebeerapp_def/modelo/ReferenciasFirebase.java
@@ -0,0 +1,16 @@
+package com.kcastilloe.thebeerapp_def.modelo;
+
+/**
+ * Clase modelo para almacenar las referencias a objetos que se encuentran en la BDD.
+ *
+ * @author Kevin Castillo Escudero
+ */
+
+public class ReferenciasFirebase {
+
+    /* Constantes que almacenan valores de referencia a nodos de la BDD en Firebase. */
+    public static final String REFERENCIA_USUARIOS = "usuarios"; /* Referencia objetos (nodos) del tipo "usuarios". */
+    public static final String REFERENCIA_CERVEZAS = "cervezas"; /* Referencia objetos (nodos) del tipo "cervezas". */
+
+
+}
