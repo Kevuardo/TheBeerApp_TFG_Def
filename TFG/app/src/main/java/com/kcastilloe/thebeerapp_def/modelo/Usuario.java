d5bf791563e56c62133ff16b5371bc84c5e7d9d3
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-11-20 11:35:05 +0100

    Añadidos campos, constructor, getters y setters

--- TFG/app/src/main/java/com/kcastilloe/thebeerapp_def/modelo/Usuario.java
+++ TFG/app/src/main/java/com/kcastilloe/thebeerapp_def/modelo/Usuario.java
@@ -8,6 +8,43 @@ package com.kcastilloe.thebeerapp_def.modelo;
 
 public class Usuario {
 
+    String nick = null;
+    String email = null;
+    int edad = 0;
 
+    /* Constructor vacío. */
+    public Usuario() {
+    }
 
+    /* COnstructor para almacenamiento en la BDD de Firebase. */
+    public Usuario(String nick, String email, int edad) {
+        this.nick = nick;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    /* Getters y Setters. */
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
