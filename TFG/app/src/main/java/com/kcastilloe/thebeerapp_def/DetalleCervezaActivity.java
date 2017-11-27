cbd323e4243064e13188a4a5c1cb165d285ff4b5
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-11-27 16:06:57 +0100

    Se añaden elementos del layout de DetalleCervezaActivity

--- TFG/app/src/main/java/com/kcastilloe/thebeerapp_def/DetalleCervezaActivity.java
+++ TFG/app/src/main/java/com/kcastilloe/thebeerapp_def/DetalleCervezaActivity.java
@@ -1,16 +1,45 @@
 package com.kcastilloe.thebeerapp_def;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.kcastilloe.thebeerapp_def.modelo.Cerveza;
+import com.kcastilloe.thebeerapp_def.modelo.Usuario;
+
+/**
+ * La actividad usada para ver los detalles de la cerveza seleccionada.
+ *
+ * @author Kevin Castillo Escudero.
+ */
+
 public class DetalleCervezaActivity extends AppCompatActivity {
+
+    private Intent intentCambio;
+    private FirebaseAuth autenticacionFirebase;
+    private FirebaseUser usuarioActual;
+    private FirebaseDatabase bddFirebase;
+    private DatabaseReference referenciaBdd;
+    private Usuario usuarioAlmacenado; /* El usuario almacenado en la BDD. */
+    private Cerveza cervezaAlmacenada; /* La cerveza almacenada en la BDD. */
+    private String idCerveza; /* La cadena identificadora de la cerveza en la BDD. */
+
+    /* Variables bandera para la navegación entre cervezas. */
+    private boolean primeraCerveza = false;
+    private boolean ultimaCerveza = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_cerveza);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); /* Fuerza la posición a vertical. */
+
+        /* Recoge la cerveza almacenada correspondiente al Token (el nombre identificador) en la BDD. */
 
     }
 }
