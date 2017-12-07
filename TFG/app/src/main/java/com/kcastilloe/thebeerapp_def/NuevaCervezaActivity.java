4b02acd8bc35d29a7deb1856115e0258ea4d8758
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-12-07 13:20:07 +0100

    Se añaden más cervezas a la BDD via NuevaCervezaActivity
    De momento es hardcode; hay que cambiar el Layout para que el usuario
    pueda insertarlas según quiera.

--- TFG/app/src/main/java/com/kcastilloe/thebeerapp_def/NuevaCervezaActivity.java
+++ TFG/app/src/main/java/com/kcastilloe/thebeerapp_def/NuevaCervezaActivity.java
@@ -46,20 +46,103 @@ public class NuevaCervezaActivity extends AppCompatActivity {
     /* Sólo testeo - genera varias cervezxas predefinidas. */
     private void generarCervezas() {
 
-        /* 1ª cerveza. */
-        nuevaCerveza = new Cerveza("Mahou", 5.5f, "Dorada con lúpulo", "España");
-        alCervezasInsercion.add(nuevaCerveza);
-
-        /* 2ª cerveza. */
-        nuevaCerveza = new Cerveza("San Miguel", 4.5f, "Dorada", "España");
-        alCervezasInsercion.add(nuevaCerveza);
-
-        /* 3ª cerveza. */
         nuevaCerveza = new Cerveza("Alhambra", 5.0f, "Dorada", "España");
         alCervezasInsercion.add(nuevaCerveza);
 
-        /* 4ª cerveza. */
+        nuevaCerveza = new Cerveza("Amstel Lager", 5.5f, "Dorada con lúpulo", "Holanda");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Asahi", 4.5f, "Dorada", "Japón");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Beck's", 5.0f, "Dorada", "EE.UU.");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Benediktiner", 5.5f, "Dorada", "Alemania");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Birra Moretti", 4.5f, "Tostada", "Italia");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Buckler", 12.5f, "Dorada", "Alemania");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Budweiser", 3.5f, "(Lluvia) Dorada", "EE.UU");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Carslberg", 5.5f, "Dorada", "Alemania");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Chimay", 8.5f, "Dorada", "Bélgica");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Chimay Red", 10.5f, "Tostada", "Bélgica");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Corona", 5.5f, "Dorada", "Argentina");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Dos Equis", 10.5f, "Dorada", "México");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Duvel", 10.5f, "Tostada con lúpulo", "República Checa");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Erdinger Weissbier", 5.5f, "Dorada", "Alemania");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Foster's", 4.5f, "Dorada", "Reino Unido");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Franziskaner", 5.5f, "Dorada de trigo", "Alemania");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Guinness", 6.5f, "Tostada con toques de café", "Irlanda");
+        alCervezasInsercion.add(nuevaCerveza);
+
         nuevaCerveza = new Cerveza("Gulden Draak", 12.5f, "Tostada con lúpulo", "Holanda");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Heineken", 4.5f, "Dorada", "Holanda");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Hoegaarden", 8.5f, "Dorada", "Bélgica");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("King Goblin", 10.5f, "Dorada con lúpulo", "República Checa");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Kirin Ichiban", 6.5f, "Dorada", "Japón");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Köstritzer", 7.5f, "Dorada", "Alemania");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Kronenbourg 1664", 5.5f, "Dorada", "Alemania");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Mahou", 5.5f, "Dorada con lúpulo", "España");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Newcastle Brown Ale", 8.5f, "Dorada", "Reino Unido");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Paulaner", 5.5f, "Dorada de trigo", "Alemania");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Peroni Nastro Azzurro", 4.5f, "Dorada", "Italia");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Presidente", 7.5f, "Dorada", "Brasil");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("San Miguel", 4.5f, "Dorada", "España");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Sapporo Premium Lager", 3.5f, "Dorada", "Japón");
+        alCervezasInsercion.add(nuevaCerveza);
+
+        nuevaCerveza = new Cerveza("Stella Artois", 9.5f, "Dorada", "República Checa");
         alCervezasInsercion.add(nuevaCerveza);
 
     }
